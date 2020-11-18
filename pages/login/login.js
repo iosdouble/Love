146011a2d58a0ff38662c6305dd288e070b631d0
iosdouble <18202504057@163.com>
2020-11-18 15:30:54 +0800

    增加用户信息管理逻辑

--- pages/login/login.js
+++ pages/login/login.js
@@ -5,14 +5,22 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    userinfo: {},
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
- 
+    console.log("加载登录页面")
+    var userInfo = wx.getStorageSync('userinfo') || {};
+    console.log("获取全局用户信息", userInfo)
+    if (Object.keys(userInfo).length!=0) {
+      wx.switchTab({
+        url: '../index/index',
+      })
+    }
+
   },
 
   /**
@@ -63,8 +71,17 @@ Page({
   onShareAppMessage: function () {
 
   },
-  userLogin:function(res){
-    console.log("点击了登录按钮",res) 
+  userLogin: function (res) {
+    var that = this
+    var userInfo = wx.getStorageSync('userinfo') || {};
+    console.log("获取全局用户信息", userInfo)
+    if (Object.keys(userInfo).length===0) {
+      console.log("点击了登录按钮", res)
+      wx.setStorage({
+        data: res,
+        key: 'userinfo',
+      })
+    }
     wx.switchTab({
       url: '../index/index',
     })
@@ -84,7 +101,7 @@ Page({
     //     })
     //   }
     // })
-  
+
   }
 
 
